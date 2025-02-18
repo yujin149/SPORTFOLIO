f761966aeebbc496821b8fbeedb8de4f748d93ae
yujin149 <swtask149@gmail.com>
2025-02-18 11:56:55 +0900

    설명 추가

--- src/main/java/com/portfolio/repository/ProjectRepository.java
+++ src/main/java/com/portfolio/repository/ProjectRepository.java
@@ -10,5 +10,6 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     //등록일 순으로 내림차순
     List<Project> findAllByOrderByRegTimeDesc();
 
+    //공지인 게시글
     List<Project> findByProjectStatusOrderByRegTimeDesc(ProjectStatus projectStatus);
 }
