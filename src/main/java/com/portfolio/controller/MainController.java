7a13508549677128ea3b4e203de09a87ef8105ec
yujin149 <swtask149@gmail.com>
2025-02-18 11:43:28 +0900

    swiper 추가

--- src/main/java/com/portfolio/controller/MainController.java
+++ src/main/java/com/portfolio/controller/MainController.java
@@ -1,14 +1,27 @@
 package com.portfolio.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.portfolio.service.ProjectService;
+import com.portfolio.entity.Project;
 
 @Controller
-@RequiredArgsConstructor
 public class MainController {
-    @GetMapping(value = "/")
-    public String mainPage() {
+    
+    private final ProjectService projectService;
+
+    @Autowired
+    public MainController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GetMapping("/")
+    public String main(Model model) {
+        List<Project> projects = projectService.getProjects();  // 프로젝트 리스트를 가져옴
+        model.addAttribute("projects", projects);  // 모델에 추가
         return "main";
     }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,4 +1,24 @@
 $(function(){
+    //swiper 추가하기
+    const swiper = new Swiper(".mainProjectSwiper", {
+        slidesPerView: 3,
+        spaceBetween: 24,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
     $(".projectListWrap .tabWrap li").click(function(){
        let tabId = $(this).children("a").attr("href");
        //alert(tabId);
@@ -7,8 +27,8 @@ $(function(){
 
         $(".projectListWrap .list").removeClass("active");
         $(tabId).addClass("active");
-
-
-
     });
+
+
+
 });
